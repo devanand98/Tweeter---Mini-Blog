b1efad097f3a445fa98ee9cf3b7f410820a1e526
Dev <devanandv12@gmail.com>
2023-01-10 16:19:26 +0000

    Added bootstrap and pagination

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
-from dotenv import load_dotenv
-from config import Config
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
-load_dotenv()
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from config import Config
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db=SQLAlchemy(app)
-migrate=Migrate(app,db)
-login=LoginManager(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-
-from app import routes, models
+from app import routes, models, errors
--- config.py
+++ config.py
@@ -2,7 +2,16 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY =os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVERE')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example']
+    POSTS_PER_PAGE = 3
+    
